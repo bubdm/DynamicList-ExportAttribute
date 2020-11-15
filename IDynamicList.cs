6a088b69464898f16d103d5839e1d4a8af6a60ef
vlad_veko <veko.2001@gmail.com>
2020-11-15 13:59:41 +0300

    List Interface added

---
+++ IDynamicList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public interface IDynamicList<T>
+    {
+        int Count { get;}
+        T this[int index]{ get; set; }
+
+        void Add(T item);
+        bool Remove(T item);
+        void RemoveAt(int index);
+        void Clear();
+    }
+}
