d2a696a954fd9048a20ffbe4f4724260dc703a65
vlad_veko <veko.2001@gmail.com>
2020-11-15 20:12:25 +0300

    Added implementation of GetTypes method

---
+++ ClassEnumerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace GenericList
+{
+    [ExportClass]
+    public static class ClassEnumerator
+    {
+        public static DynamicList<Type> GetPublicTypes(Assembly asm)
+        {
+            var types = new DynamicList<Type>();
+            foreach (Type type in asm.GetTypes().Where(typeVal => Attribute.IsDefined(typeVal, typeof(ExportClass)) && typeVal.IsClass && typeVal.IsPublic))
+            {
+                types.Add(type);
+            }
+
+            return types;
+        }
+    }
+}
